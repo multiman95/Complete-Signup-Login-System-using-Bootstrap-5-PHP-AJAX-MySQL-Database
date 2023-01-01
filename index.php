977e1b24cce82f70091e1b7c4203e7f79a68e7da
MultiMaN95 <84718035+multiman95@users.noreply.github.com>
2023-01-01 12:02:03 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,65 @@
-!DOCTYPE html
-<html lang='en'>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
 
-</html
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+	<h1 class="text-center mt-5">Login Page</h1>
+		
+		<div class="container">
+			
+			<div class="card">
+				<div class="card-body bg-success">
+					
+					<div class="d-flex justify-content-center">
+						<form id="loginForm">
+
+						<div class="form-group col-lg-12">
+							<label class="text-white">Email</label>
+							<input type="text" id="userEmail" class="form-control">
+						</div>
+
+						<br/>
+						
+						<div class="form-group col-lg-12">
+							<label class="text-white">Password</label>
+							<input type="password" id="userPassword" class="form-control">
+						</div>
+
+						<div class="d-grid gap-2 mt-3">
+							<button class="btn btn-primary btn-block">
+							Login
+							</button>
+						</div>
+
+						<center>
+							<p class="mt-4">New User?
+								<a href="sigup.php" class="text-white">
+									Signup
+								</a>
+							</p>
+
+							<p class="mt-4">Forgot Password? 
+								<a href="forgot-password.php" class="text-white">
+									Reset Password
+								</a>
+							</p>
+						</center>
+
+					</form>
+
+					
+					</div>
+
+				</div>
+			</div>
+
+	</div>
+
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
